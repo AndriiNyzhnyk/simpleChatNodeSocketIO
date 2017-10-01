2adf6738fab54ce20985a7b9736b8f191fb79480
Andrii Nyzhnyk <andrij1510@gmail.com>
2017-10-01 22:35:05 +0300

    add error handler 404 and 500

--- server.js
+++ server.js
@@ -28,6 +28,22 @@ io.on('connection', (socket) => {
     });
 });
 
+// Обробник 404 помилки
+app.use((req, res, next) => {
+    res.status(404);
+    res.type("text/plain");
+    res.send("404");
+});
+
+// Обробник 505 помилки
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500);
+    res.type("text/plain");
+    res.send("500");
+});
+
+
 server.listen(app.get('port'), () => {
     console.log( 'Express запущенний на http://localhost:' +
         app.get('port') + '; нажміть Ctrl+C для завершення.' );
